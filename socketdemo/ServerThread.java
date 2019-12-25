3bc80b73b066e095cabf87cf11b4c82926144ee4
chun-29 <54794957+chun-29@users.noreply.github.com>
2019-12-25 22:18:14 +0800

    Update ServerThread.java

--- socketdemo/ServerThread.java
+++ socketdemo/ServerThread.java
@@ -4,8 +4,15 @@ import java.io.*;
 import java.net.InetAddress;
 import java.net.Socket;
 
+/**
+ * @program: classprogram
+ * @Description: ServerThread
+ * @author: chun
+ * @date: 2019/12/9 下午
+ */
 public class ServerThread extends Thread {
     Socket socket = null;
+    //接收客户端的连接
     InetAddress internetAddress = null;
     public ServerThread(Socket socket,InetAddress inetAddress) {
         this.socket = socket;
@@ -14,20 +21,30 @@ public class ServerThread extends Thread {
 
     @Override
     public void run() {
+        //字节输入流
         InputStream inputStream = null;
+        //将一个字节流中的字节解码成字符
         InputStreamReader inputStreamReader = null;
+        //为输入流添加缓冲
         BufferedReader bufferedReader = null;
+        //字节输出流
         OutputStream outputStream = null;
+        //将写入的字符编码成字节后写入一个字节流
         OutputStreamWriter writer = null;
         try {
             inputStream = socket.getInputStream();
             inputStreamReader = new InputStreamReader(inputStream,"UTF-8");
             bufferedReader = new BufferedReader(inputStreamReader);
+            //临时
             String info = null;
+            //循环读取客户端信息
             while ((info = bufferedReader.readLine()) != null) {
-                System.out.println("服务器端接收:" + "{'from_client':'"+ socket.getInetAddress().getHostAddress()+"','data':'"+info+"'}");
+                //获取客户端的ip地址及发送数据
+                System.out.println("服务器端接收:" + "{'from_client':'"+socket.getInetAddress().getHostAddress()+"','data':'"+info+"'}");
             }
+            //关闭输入流
             socket.shutdownInput();
+            //响应客户端请求
             outputStream = socket.getOutputStream();
             writer = new OutputStreamWriter(outputStream,"UFT-8");
             writer.write("{'to_client':'"+internetAddress.getHostAddress()+"','data':'我是服务器数据'}");
@@ -35,6 +52,7 @@ public class ServerThread extends Thread {
         }catch (IOException e) {
             e.printStackTrace();
         }finally {
+            //关闭资源
             try {
                 if(writer != null) {
                     writer.close();
