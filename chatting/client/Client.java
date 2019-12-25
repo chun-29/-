88c9b3bfe24d6c95d127a77187c3d230ca7a2214
chun-29 <54794957+chun-29@users.noreply.github.com>
2019-12-25 21:17:46 +0800

    Update Client.java

--- chatting/client/Client.java
+++ chatting/client/Client.java
@@ -6,8 +6,15 @@ import com.google.chapter.chatting.threadchat.SocketWriter;
 import java.io.IOException;
 import java.net.Socket;
 
+/**
+ * @program: classprogram
+ * @Description: 通信
+ * @author: chun
+ * @date: 2019/12/11 下午
+ */
 public class Client {
     public static void main(String[] args) throws IOException {
+        //ip  和  端口号
         Socket socket = new Socket("127.0.0.1",8888);
         Thread send = new Thread(new SocketWriter(socket,"我"));
         Thread reception = new Thread(new SocketReader(socket));
