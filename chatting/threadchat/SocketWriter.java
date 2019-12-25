49aeb08d6d5e59a3dd763f0be2603f7b97e380c9
chun-29 <54794957+chun-29@users.noreply.github.com>
2019-12-25 22:03:00 +0800

    Update SocketWriter.java

--- chatting/threadchat/SocketWriter.java
+++ chatting/threadchat/SocketWriter.java
@@ -6,6 +6,12 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
 
+/**
+ * @program: classprogram
+ * @Description: 通信
+ * @author: chun
+ * @date: 2019/12/11 下午
+ */
 public class SocketWriter implements Runnable {
     private Socket socket;
     private String userName;
@@ -14,15 +20,30 @@ public class SocketWriter implements Runnable {
         this.socket = socket;
         this.userName = userName;
     }
+
+     /**
+     * When an object implementing interface <code>Runnable</code> is used
+     * to create a thread, starting the thread causes the object's
+     * <code>run</code> method to be called in that separately executing
+     * thread.
+     * <p>
+     * The general contract of the method <code>run</code> is that it may
+     * take any action whatsoever.
+     *
+     * @see Thread#run()
+     */
     @Override
     public void run() {
         String temp = new String();
         Scanner input = new Scanner(System.in);
         try {
+            //打印流 通过Socket用于发送消息
             pw = new PrintWriter(socket.getOutputStream(),true);
             while (true) {
                 temp = input.nextLine();
+                //将消息发送出去
                 pw.println(userName+ ":"+ temp);
+                //以bye结尾就退出
                 if (temp.endsWith("bye")) {
                     break;
                 }
